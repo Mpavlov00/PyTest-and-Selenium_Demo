c8cd9bceac26b137e4460fdbca62349340ad5c02
Mpavlov00 <mishpavlov000@gmail.com>
2022-10-04 14:48:26 +0300

    изменен формат обращения к веб драйверу

--- Sandbox_shop_testing/conftest.py
+++ Sandbox_shop_testing/conftest.py
@@ -2,23 +2,20 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')
+    options.add_argument('--statr-maximized')
+    options.add_argument('--window-size = 1650,900')
+    return options
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield browser
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(executable_path='E:\GitRepo\PyTest-and-Selenium_Demo\Sandbox_shop_testing\chromedriver', options=options)
+    yield driver
     print("\nquit browser..")
-    browser.quit()
+    driver.quit()
